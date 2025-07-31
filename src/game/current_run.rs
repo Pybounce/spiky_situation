40b18dd7accf2b6859157623e69f5f2455ad2021
Pybounce <pybounce@protonmail.com>
2025-07-31 04:42:11 +0100

    improved UI

--- src/game/current_run.rs
+++ src/game/current_run.rs
@@ -50,7 +50,7 @@ pub fn update_lives_remaining_text(
 ) {
     if let Some(current_run) = current_run_opt {
         for mut text in &mut query {
-            text.sections[0].value = current_run.lives_remaining().to_string();
+            text.sections[0].value = format!("Lives: {}", current_run.lives_remaining());
         }
     }
 }
@@ -61,7 +61,7 @@ pub fn update_stages_complete(
 ) {
     if let Some(current_run) = current_run_opt {
         for mut text in &mut query {
-            text.sections[0].value = current_run.stages_complete.to_string();
+            text.sections[0].value = format!("Score: {}", current_run.stages_complete);
         }
     }
 }
@@ -69,22 +69,42 @@ pub fn update_stages_complete(
 pub fn add_current_run_ui(
     mut commands: Commands
 ) {
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("-", TextStyle::default()),
-            transform: Transform::from_translation(Vec3::new(0.0, 0.0, 20.0)),
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                position_type: PositionType::Absolute,
+                top: Val::Px(20.0),
+                left: Val::Px(20.0),
+                flex_direction: FlexDirection::Column,
+                row_gap: Val::Px(15.0),
+                ..default()
+            },
             ..default()
-        },
-        LivesRemainingText
-    ))
-    .insert(DespawnOnStateExit::App(AppState::Game));
-    
-    commands.spawn((
-        Text2dBundle {
-            text: Text::from_section("-", TextStyle::default()),
-            ..default()
-        },
-        StagesCompleteText
-    ))
-    .insert(DespawnOnStateExit::App(AppState::Game));
+        })
+        .with_children(|parent| {
+            parent.spawn((
+                TextBundle::from_section(
+                "complete: 3",
+                TextStyle {
+                    font_size: 28.0,
+                    color: Color::WHITE,
+                    ..default()
+                }),
+                StagesCompleteText,
+                DespawnOnStateExit::App(AppState::Game)
+            ));
+
+            parent.spawn((
+                TextBundle::from_section(
+                "Lives: 3",
+                TextStyle {
+                    font_size: 28.0,
+                    color: Color::WHITE,
+                    ..default()
+                }),
+                LivesRemainingText,
+                DespawnOnStateExit::App(AppState::Game)
+            ));
+        });
+
 }
