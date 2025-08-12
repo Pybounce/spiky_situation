dad685d61b7dde4a7c0ae51a61618b98791628e4
Pybounce <pybounce@protonmail.com>
2025-08-12 16:32:56 +0100

    cam border UI

--- src/game/endless/ui.rs
+++ src/game/endless/ui.rs
@@ -19,41 +19,118 @@ pub fn add_endless_mode_ui(
 ) {
     commands
         .spawn(Node {
-            position_type: PositionType::Absolute,
-            top: Val::Px(50.0),
-            left: Val::Px(50.0),
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
             flex_direction: FlexDirection::Column,
-            row_gap: Val::Px(15.0),
+            justify_content: JustifyContent::SpaceBetween,
+            align_content: AlignContent::SpaceBetween,
+            border: UiRect::all(Val::Percent(4.0)),
             ..default()
         })
         .with_children(|parent| {
-            parent.spawn((
-                Node {
-                    ..default()
-                },
-                TextFont {
-                    font_size: 28.0,
-                    ..default()
-                },
-                TextColor(Color::WHITE),
-                Text::new(""),
-                EndlessStagesCompleteText,
-                DespawnOnStateExit::App(AppState::Game)
-            ));
 
-            parent.spawn((
+            parent.spawn(
                 Node {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(50.0),
+                    justify_content: JustifyContent::SpaceBetween,
                     ..default()
-                },
-                TextFont {
-                    font_size: 28.0,
+                }
+            ).with_children(|top_row_parent| {
+                top_row_parent.spawn((
+                    Node {
+                        min_width: Val::Px(150.),
+                        min_height: Val::Px(150.),
+                        max_width: Val::Percent(5.),
+                        max_height: Val::Percent(5.),
+                        width: Val::Auto,
+                        height: Val::Auto,
+                        border: UiRect::left(Val::Px(5.0)).with_top(Val::Px(5.0)),
+                        ..default()
+                    },
+                    BorderColor(Color::WHITE),
+
+                ));
+                top_row_parent.spawn((
+                    Node {
+                        min_width: Val::Px(150.),
+                        min_height: Val::Px(150.),
+                        max_width: Val::Percent(5.),
+                        max_height: Val::Percent(5.),
+                        width: Val::Auto,
+                        height: Val::Auto,
+                        border: UiRect::right(Val::Px(5.0)).with_top(Val::Px(5.0)),
+                        ..default()
+                    },
+                    BorderColor(Color::WHITE),
+
+                ));
+            });
+            parent.spawn(
+                Node {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(50.0),
+                    justify_content: JustifyContent::SpaceBetween,
+                    align_items: AlignItems::FlexEnd,
                     ..default()
-                },
-                TextColor(Color::WHITE),
-                Text::new(""),
-                EndlessLivesRemainingText,
-                DespawnOnStateExit::App(AppState::Game)
-            ));
+                }
+            ).with_children(|bottom_row_parent| {
+                bottom_row_parent.spawn((
+                    Node {
+                        min_width: Val::Px(150.),
+                        min_height: Val::Px(150.),
+                        max_width: Val::Percent(5.),
+                        max_height: Val::Percent(5.),
+                        width: Val::Auto,
+                        height: Val::Auto,
+                        border: UiRect::left(Val::Px(5.0)).with_bottom(Val::Px(5.0)),
+                        ..default()
+                    },
+                    BorderColor(Color::WHITE),
+                ));
+                bottom_row_parent.spawn((
+                    Node {
+                        min_width: Val::Px(150.),
+                        min_height: Val::Px(150.),
+                        max_width: Val::Percent(5.),
+                        max_height: Val::Percent(5.),
+                        width: Val::Auto,
+                        height: Val::Auto,
+                        border: UiRect::right(Val::Px(5.0)).with_bottom(Val::Px(5.0)),
+                        ..default()
+                    },
+                    BorderColor(Color::WHITE),
+
+                ));
+            });
+
+            //parent.spawn((
+            //    Node {
+            //        ..default()
+            //    },
+            //    TextFont {
+            //        font_size: 28.0,
+            //        ..default()
+            //    },
+            //    TextColor(Color::WHITE),
+            //    Text::new(""),
+            //    EndlessStagesCompleteText,
+            //    DespawnOnStateExit::App(AppState::Game)
+            //));
+//
+            //parent.spawn((
+            //    Node {
+            //        ..default()
+            //    },
+            //    TextFont {
+            //        font_size: 28.0,
+            //        ..default()
+            //    },
+            //    TextColor(Color::WHITE),
+            //    Text::new(""),
+            //    EndlessLivesRemainingText,
+            //    DespawnOnStateExit::App(AppState::Game)
+            //));
         });
 
 }
