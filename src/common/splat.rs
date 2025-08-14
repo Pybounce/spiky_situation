f9f3b485ad3ebb1bc06bf03f1f24c0ae61db2982
Pybounce <pybounce@protonmail.com>
2025-08-14 14:18:46 +0100

    setting z coord of splat

--- src/common/splat.rs
+++ src/common/splat.rs
@@ -20,7 +20,6 @@ pub fn apply_splat_on_death(
     splat_db: Res<SplatDb>
 ) {
     let mut rng = rand::thread_rng();
-
     for transform in &query {
         for _ in 0..2 {
             let (splat_tex, splat_rect, origin_offset) = splat_db.random_radial();
@@ -34,12 +33,12 @@ pub fn apply_splat_on_death(
                 brightness: rng.gen_range(0.8..1.2)
             });
             let mesh = meshes.add(Mesh::from(Rectangle::default()));
-
+            let translation = transform.translation - origin_offset.extend(0.0);
             commands.spawn((
                 Mesh2d(mesh),
                 MeshMaterial2d(splat_mat),
                 Transform { 
-                    translation: transform.translation - (Vec3::Z * 10.0) - origin_offset.extend(0.0), 
+                    translation: translation.with_z(-10.0), 
                     scale: Vec3::new(splat_rect.width(), splat_rect.height(), 1.0),
                     rotation: Quat::from_rotation_z(rng.gen_range(0..4) as f32 * FRAC_PI_2 * 0.0),
                     ..default()
