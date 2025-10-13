cd8d650212bb84feda753f222a71fe662bcbc0e8
Pybounce <pybounce@protonmail.com>
2025-10-13 23:51:02 +0100

    removed temporal accumulation :(

--- src/rt_lights/mod.rs
+++ src/rt_lights/mod.rs
@@ -2,7 +2,7 @@
 use bevy::{prelude::*, render::RenderApp};
 use bevy_app_compute::prelude::*;
 
-use crate::rt_lights::{compute_shader::{extract_lighting_out_buffer, increment_temporal_lightmap_index, init_occluder_mask, refresh_static_occluders, update_occluder_current_frame, update_rt_lights, write_occluder_buffer, RTLComputeWorker, TemporalLightmapIndex}, occluders::setup_occluder_map, post_process_shader::RTLPostProcessPlugin};
+use crate::rt_lights::{compute_shader::{extract_lighting_out_buffer, init_occluder_mask, refresh_static_occluders, update_occluder_current_frame, update_rt_lights, write_occluder_buffer, RTLComputeWorker}, occluders::setup_occluder_map, post_process_shader::RTLPostProcessPlugin};
 
 pub(crate) mod compute_shader;
 pub(crate) mod post_process_shader;
@@ -19,9 +19,8 @@ impl Plugin for RTLightPlugin {
         app.add_plugins(AppComputePlugin)
         .add_plugins(AppComputeWorkerPlugin::<RTLComputeWorker>::default())
         .add_plugins(RTLPostProcessPlugin)
-        .init_resource::<TemporalLightmapIndex>()
         .add_systems(Startup, (init_occluder_mask).chain())
-        .add_systems(PreUpdate, (update_occluder_current_frame, refresh_static_occluders, increment_temporal_lightmap_index))
+        .add_systems(PreUpdate, (update_occluder_current_frame, refresh_static_occluders))
         .add_systems(Update, (write_occluder_buffer, update_rt_lights));
 
     app.sub_app_mut(RenderApp)
