116d98ebf9ef269658547ee1acfb5c4d60255215
Pybounce <pybounce@protonmail.com>
2026-03-16 12:23:33 +0000

    player wall slide sfx but they suck

--- src/builders/player_builders.rs
+++ src/builders/player_builders.rs
@@ -2,7 +2,7 @@ use bevy::{ecs::system::EntityCommands, platform::collections::HashMap, prelude:
 use avian2d::prelude::*;
 use bevy_seedling::{prelude::{SpatialBasicNode, Volume, VolumeNode}, sample::SamplePlayer, sample_effects};
 
-use crate::{common::{animated_sprite::SpriteAnimator, animation_controller::{AnimationController, AnimationState}, death::{DelayedDeathMarker, Killable}, physics::{avian_ex::ManyCollidingEntities, gravity::Gravity, layers::GamePhysicsLayer}, splat::SplatOnDeath}, ground::Groundable, lit_sprite::global_components::LitSprite, local_player::{LocalPlayer, PLAYER_MAX_GRAVITY, PLAYER_SIZE}, player::{animation::PlayerAnimationState, dash_controller::DashController, death::Respawnable, horizontal_movement_controller::{AirbourneHorizontalMovementController, GroundedHorizontalMovementController}, jump_controller::JumpController, physics_controller::PhysicsController, wall_jump_controller::{WallJumpController, WallStickable}}, rt_lights::components::LightOccluder, stage::{stage_builder::stage_creator::TILE_SIZE, stage_objects::StageObject}, wall::Wallable};
+use crate::{common::{animated_sprite::SpriteAnimator, animation_controller::{AnimationController, AnimationState}, death::{DelayedDeathMarker, Killable}, physics::{avian_ex::ManyCollidingEntities, gravity::Gravity, layers::GamePhysicsLayer}, splat::SplatOnDeath}, ground::Groundable, lit_sprite::global_components::LitSprite, local_player::{LocalPlayer, PLAYER_MAX_GRAVITY, PLAYER_SIZE}, player::{animation::PlayerAnimationState, audio::{FootstepAudioEmitter, WallSlideAudioEmitter}, dash_controller::DashController, death::Respawnable, horizontal_movement_controller::{AirbourneHorizontalMovementController, GroundedHorizontalMovementController}, jump_controller::JumpController, physics_controller::PhysicsController, wall_jump_controller::{WallJumpController, WallStickable}}, rt_lights::components::LightOccluder, stage::{stage_builder::stage_creator::TILE_SIZE, stage_objects::StageObject}, wall::Wallable};
 use crate::common::player_input::{gamepad::PlayerGamepadInput, keyboard::PlayerKeyboardInput, PlayerInputController};
 use crate::local_player::*;
 
@@ -81,6 +81,19 @@ impl PlayerBuilder {
         state_animations.insert(AnimationState(PlayerAnimationState::Hovering as u32), hovering_rects.clone());
         state_animations.insert(AnimationState(PlayerAnimationState::OnWall as u32), wall_grab_rects.clone());
         state_animations.insert(AnimationState(PlayerAnimationState::Dancing as u32), dance_rects.clone());
+
+        let wall_slide_emitter = entity_commands.commands_mut().spawn((SamplePlayer::new(asset_server.load("audio/sfx/wall_slide.ogg")).looping(),
+            sample_effects![VolumeNode {
+                volume: Volume::Linear(0.0),
+                ..default()
+            }]
+        )).id();
+        let running_emitter = entity_commands.commands_mut().spawn((SamplePlayer::new(asset_server.load("audio/sfx/running.ogg")).looping(),
+            sample_effects![VolumeNode {
+                volume: Volume::Linear(0.0),
+                ..default()
+            }]
+        )).id();
 
         entity_commands.try_insert(((
             LocalPlayer,
@@ -170,12 +183,12 @@ impl PlayerBuilder {
                 jump_key: KeyCode::KeyW,
                 dash_key: KeyCode::Space,
             }),
-            (SamplePlayer::new(asset_server.load("audio/sfx/running.ogg")).looping(),
-            sample_effects![VolumeNode {
-                volume: Volume::Linear(0.0),
-                ..default()
-            }])
+            (WallSlideAudioEmitter(wall_slide_emitter),
+            FootstepAudioEmitter(running_emitter))
         ));
+
+        entity_commands.add_child(wall_slide_emitter);
+        entity_commands.add_child(running_emitter);
 
     }
 }
--- src/player/audio.rs
+++ src/player/audio.rs
@@ -3,22 +3,47 @@ use bevy::prelude::*;
 use bevy_seedling::prelude::{EffectsQuery, SampleEffects, Volume, VolumeNode};
 use avian2d::prelude::LinearVelocity;
 
-use crate::{ground::Grounded, local_player::MAX_HORIZONTAL_SPEED};
+use crate::{ground::Grounded, local_player::MAX_HORIZONTAL_SPEED, wall::TouchingWall};
+
+
+#[derive(Component)]
+pub struct WallSlideAudioEmitter(pub Entity);
+
+#[derive(Component)]
+pub struct FootstepAudioEmitter(pub Entity);
 
 pub fn control_player_footstep_audio(
-    query: Query<(&SampleEffects, &LinearVelocity, Option<&Grounded>)>,
+    query: Query<(&FootstepAudioEmitter, &LinearVelocity, Option<&Grounded>)>,
+    effects_query: Query<&SampleEffects>,
     mut volume_query: Query<&mut VolumeNode>,
 ) {
-    for (effects, linvel, grounded_opt) in query.iter() {
-
+    for (emitter, linvel, grounded_opt) in query.iter() {
+        let Ok(effects) = effects_query.get(emitter.0) else { continue; };
         let Ok(mut volume) = volume_query.get_effect_mut(effects) else { continue; };
 
         let mut target_volume = 0.0;
         let max_volume = 1.0;
         if grounded_opt.is_some() {
-            target_volume = 0.0.lerp(max_volume, linvel.x.abs() / MAX_HORIZONTAL_SPEED);
+            target_volume = 0.0.lerp(max_volume, linvel.x.abs() / MAX_HORIZONTAL_SPEED).min(max_volume);
         }
         volume.volume = Volume::Linear(target_volume);
     }
 }
 
+pub fn control_player_wall_slide_audio(
+    query: Query<(&WallSlideAudioEmitter, &LinearVelocity, Option<&TouchingWall>)>,
+    effects_query: Query<&SampleEffects>,
+    mut volume_query: Query<&mut VolumeNode>,
+) {
+    for (emitter, linvel, grounded_opt) in query.iter() {
+        let Ok(effects) = effects_query.get(emitter.0) else { continue; };
+        let Ok(mut volume) = volume_query.get_effect_mut(effects) else { continue; };
+
+        let mut target_volume = 0.0;
+        let max_volume = 0.5;
+        if grounded_opt.is_some() {
+            target_volume = 0.0.lerp(max_volume, linvel.y.abs() / MAX_HORIZONTAL_SPEED).min(max_volume);    //TODO: Max horizontal speed should not be here but unlike running, wall slide has no max, it's friction based.
+        }
+        volume.volume = Volume::Linear(target_volume);
+    }
+}
